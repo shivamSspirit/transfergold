f86ed08d3ecaaeab01cdc887debaff81bd88a7af
shivam <shivam.spirite3@outlook.com>
2023-12-04 19:25:18 +0530

    adding

--- solidity/transfergold.sol
+++ solidity/transfergold.sol
@@ -1,64 +1,121 @@
 import "../libraries/spl_token.sol";
 import "../libraries/mpl_metadata.sol";
 
-@program_id("29xSEMAjUToGKYuoZzUHW8AsfkkPrRhnJVn8WKoarMtv")
+@program_id("8gyzRD6DgQo7twC8mnieALv8EbD9Ait3U1hMHKvueoWo")
 contract spl_token_minter {
     @payer(payer)
     constructor() {}
 
+
+    @mutableSigner(payer) // payer account
+    @mutableSigner(mint) // mint account to be created
+    @mutableAccount(metadata) // metadata account to be created
+    @signer(mintAuthority) // mint authority for the mint account
+    @account(rentAddress)
+    @account(metadataProgramId)
     function createTokenMint(
-        address payer, // payer account
-        address mint, // mint account to be created
-        address mintAuthority, // mint authority for the mint account
         address freezeAuthority, // freeze authority for the mint account
-        address metadata, // metadata account to be created
         uint8 decimals, // decimals for the mint account
         string name, // name for the metadata account
         string symbol, // symbol for the metadata account
         string uri // uri for the metadata account
-    ) public {
+    ) external {
         // Invoke System Program to create a new account for the mint account and,
         // Invoke Token Program to initialize the mint account
         // Set mint authority, freeze authority, and decimals for the mint account
         SplToken.create_mint(
-            payer,            // payer account
-            mint,            // mint account
-            mintAuthority,   // mint authority
+            tx.accounts.payer.key,            // payer account
+            tx.accounts.mint.key,            // mint account
+            tx.accounts.mintAuthority.key,   // mint authority
             freezeAuthority, // freeze authority
             decimals         // decimals
         );
 
         // Invoke Metadata Program to create a new account for the metadata account
+       // Invoke Metadata Program to create a new account for the metadata account
         MplMetadata.create_metadata_account(
-            metadata, // metadata account
-            mint,  // mint account
-            mintAuthority, // mint authority
-            payer, // payer
-            payer, // update authority (of the metadata account)
+            tx.accounts.metadata.key, // metadata account
+            tx.accounts.mint.key,  // mint account
+            tx.accounts.mintAuthority.key, // mint authority
+            tx.accounts.payer.key, // payer
+            tx.accounts.payer.key, // update authority (of the metadata account)
             name, // name
             symbol, // symbol
-            uri // uri (off-chain metadata json)
+            uri, // uri (off-chain metadata json)
+            tx.accounts.rentAddress.key,
+            tx.accounts.metadataProgramId.key
         );
+
+        // Invoke Metadata Program to create a new account for the metadata account
+        // MplMetadata.create_metadata_account(
+        //     tx.accounts.metadata.key, // metadata account
+        //     tx.accounts.mint.key,  // mint account
+        //     mintAuthority, // mint authority
+        //     payer, // payer
+        //     payer, // update authority (of the metadata account)
+        //     name, // name
+        //     symbol, // symbol
+        //     uri // uri (off-chain metadata json)
+        // );
     }
 
-    function mintTo(address mintAuthority, address tokenAccount, address mint, uint64 amount) public {
+    // // create ata 
+    // @mutableSigner(payer) // payer account
+    // @mutableAccount(tokenAccount)
+    // @mutableAccount(mint) // mint account to be created
+    // @mutableSigner(owner) // owner
+    // function createAssociatedTokenAccount() external {
+    //     SplToken.create_associated_token_account(
+    //         tx.accounts.payer.key,
+    //         tx.accounts.tokenAccount.key,
+    //         tx.accounts.mint.key,
+    //         tx.accounts.owner.key
+    //     );
+    // }
+
+    @mutableAccount(mint)
+    @mutableAccount(tokenAccount)
+    @mutableSigner(mintAuthority)
+    function mintTo(uint64 amount) external {
         // Mint tokens to the token account
         SplToken.mint_to(
-            mint, // mint account
-            tokenAccount, // token account
-            mintAuthority, // mint authority
+            tx.accounts.mint.key, // mint account
+            tx.accounts.tokenAccount.key, // token account
+            tx.accounts.mintAuthority.key, // mint authority
             amount // amount
         );
     }
 
 
+
      // Transfer tokens from one token account to another via Cross Program Invocation to Token Program
+    // function transferTokens(
+    //     address from, // token account to transfer from
+    //     address to, // token account to transfer to
+    //     uint64 amount // amount to transfer
+    // ) public {
+    //     SplToken.TokenAccountData from_data = SplToken.get_token_account_data(from);
+    //     SplToken.transfer(from, to, from_data.owner, amount);
+    // }
+
+
+
+
+    // Transfer tokens from one token account to another via Cross Program Invocation to Token Program
+    @mutableAccount(from) // token account to transfer from
+    @mutableAccount(to) // token account to transfer to
+    @signer(owner)
     function transferTokens(
-        address from, // token account to transfer from
-        address to, // token account to transfer to
         uint64 amount // amount to transfer
-    ) public {
-        SplToken.TokenAccountData from_data = SplToken.get_token_account_data(from);
-        SplToken.transfer(from, to, from_data.owner, amount);
+    ) external {
+        SplToken.transfer(
+            tx.accounts.from.key, 
+            tx.accounts.to.key, 
+            tx.accounts.owner.key, 
+            amount
+        );
     }
+
+
+
 }
--- tests/transfergold.ts
+++ tests/transfergold.ts
@@ -1,40 +1,42 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SplTokenMinter } from "../target/types/spl_token_minter";
-import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, LAMPORTS_PER_SOL,ParsedAccountData } from "@solana/web3.js";
+import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { Metaplex } from "@metaplex-foundation/js";
 import { assert } from "chai";
-
-
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddressSync,
   getOrCreateAssociatedTokenAccount,
   TOKEN_PROGRAM_ID,
   getAccount
 } from "@solana/spl-token";
+import { WalletConfigError } from "@solana/wallet-adapter-base";
 
-describe("spl-token-minter", () => {
+describe("Transfer Tokens", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  // Generate a new keypair for the data account for the program
   const dataAccount = anchor.web3.Keypair.generate();
-  // Generate a mint keypair
   const mintKeypair = anchor.web3.Keypair.generate();
   const wallet = provider.wallet as anchor.Wallet;
   const connection = provider.connection;
 
   const program = anchor.workspace.SplTokenMinter as Program<SplTokenMinter>;
 
-  // Metadata for the Token
-  const tokenTitle = "Solana pro";
-  const tokenSymbol = "GM";
+  const tokenTitle = "Kingmans";
+  const tokenSymbol = "KING";
   const tokenUri =
-    "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json";
+    "https://res.cloudinary.com/ddwkxn8ak/image/upload/v1698823073/solangsol/Course1_mhz1c1.png";
+
+  let tokenAccount;
+
+
+
 
   it("Is initialized!", async () => {
-    // Initialize data account for the program, which is required by Solang
+    // Add your test here.
     const tx = await program.methods
       .new()
       .accounts({ dataAccount: dataAccount.publicKey })
@@ -43,156 +45,72 @@ describe("spl-token-minter", () => {
     console.log("Your transaction signature", tx);
   });
 
-
-
   it("Create an SPL Token!", async () => {
-    // Get the metadata address for the mint
     const metaplex = Metaplex.make(connection);
     const metadataAddress = await metaplex
       .nfts()
       .pdas()
       .metadata({ mint: mintKeypair.publicKey });
 
-    // Create the token mint
+    // Add your test here.
     const tx = await program.methods
       .createTokenMint(
-        wallet.publicKey, // payer
-        mintKeypair.publicKey, // mint
-        wallet.publicKey, // mint authority
         wallet.publicKey, // freeze authority
-        metadataAddress, // metadata address
-        9, // decimals
-        tokenTitle, // token name
-        tokenSymbol, // token symbol
-        tokenUri // token uri
+        9, // 0 decimals for NFT
+        tokenTitle, // NFT name
+        tokenSymbol, // NFT symbol
+        tokenUri // NFT URI
       )
-      .accounts({ dataAccount: dataAccount.publicKey })
-      .remainingAccounts([
-        {
-          pubkey: wallet.publicKey,
-          isWritable: true,
-          isSigner: true,
-        },
-        { pubkey: mintKeypair.publicKey, isWritable: true, isSigner: true },
-        {
-          pubkey: new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"), // Metadata program id
-          isWritable: false,
-          isSigner: false,
-        },
-        { pubkey: metadataAddress, isWritable: true, isSigner: false },
-        { pubkey: SystemProgram.programId, isWritable: false, isSigner: false },
-        { pubkey: SYSVAR_RENT_PUBKEY, isWritable: false, isSigner: false },
-      ])
+      .accounts({
+        payer: wallet.publicKey,
+        mint: mintKeypair.publicKey,
+        metadata: metadataAddress,
+        mintAuthority: wallet.publicKey,
+        rentAddress: SYSVAR_RENT_PUBKEY,
+        metadataProgramId: new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"),
+      })
       .signers([mintKeypair])
       .rpc({ skipPreflight: true });
     console.log("Your transaction signature", tx);
   });
 
+
   it("Mint some tokens to your wallet!", async () => {
     // Wallet's associated token account address for mint
-    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+
+
+    console.log("wallet",wallet)
+    tokenAccount = await getOrCreateAssociatedTokenAccount(
       connection,
       wallet.payer, // payer
       mintKeypair.publicKey, // mint
       wallet.publicKey // owner
     );
 
-    console.log("sendertokenAccount", tokenAccount)
+
+
+    console.log("associated token account:", tokenAccount)
 
     const tx = await program.methods
       .mintTo(
-        wallet.publicKey, // payer
-        tokenAccount.address, // associated token account address
-        mintKeypair.publicKey, // mint
-        new anchor.BN(199000000000) // amount to mint
+        new anchor.BN(150) // amount to mint
       )
-      .accounts({ dataAccount: dataAccount.publicKey })
-      .remainingAccounts([
-        {
-          pubkey: wallet.publicKey,
-          isWritable: true,
-          isSigner: true,
-        },
-        { pubkey: tokenAccount.address, isWritable: true, isSigner: false },
-        { pubkey: mintKeypair.publicKey, isWritable: true, isSigner: false },
-        {
-          pubkey: SystemProgram.programId,
-          isWritable: false,
-          isSigner: false,
-        },
-        { pubkey: TOKEN_PROGRAM_ID, isWritable: false, isSigner: false },
-        {
-          pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
-          isWritable: false,
-          isSigner: false,
-        },
-      ])
+      .accounts({
+        mintAuthority: wallet.publicKey,
+        tokenAccount: tokenAccount.address,
+        mint: mintKeypair.publicKey,
+      })
       .rpc({ skipPreflight: true });
     console.log("Your transaction signature", tx);
 
-    // Get the minted token amount on the associated token account
-    const tokenAmount = (await getAccount(connection, tokenAccount.address)).amount;
-    console.log("tokenAmount", tokenAmount);
-    // Converting tokenAmount to a regular number using Number()
-    let tokens = Number(tokenAmount);
-    console.log("minted token amounts", tokens / LAMPORTS_PER_SOL);
-    assert.equal(tokens / LAMPORTS_PER_SOL, 199);
+    // console.log("Your transaction signature", tx);
+    const recepienttokenAmount = (await getAccount(connection, tokenAccount.address)).amount;
+    console.log("recipienttokenAmount", recepienttokenAmount);
+    let tokens = Number(recepienttokenAmount);
+    console.log("tkens", tokens)
   });
 
 
-// transfer token to another wallet via cpi
-
-it("Transfer some tokens to another wallet!", async () => {
-  // Wallet's associated token account address for mint
-  const tokenAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    wallet.payer, // payer
-    mintKeypair.publicKey, // mint
-    wallet.publicKey // owner
-  );
-
-  const receipient = anchor.web3.Keypair.generate();
-  const receipientTokenAccount = await getOrCreateAssociatedTokenAccount(
-    connection,
-    wallet.payer, // payer
-    mintKeypair.publicKey, // mint account
-    receipient.publicKey // owner account
-  );
-
-  console.log("receipientTokenAccount",receipientTokenAccount)
-
-  const tx = await program.methods
-    .transferTokens(
-      tokenAccount.address,
-      receipientTokenAccount.address,
-      new anchor.BN(54000000000)
-    )
-    .accounts({ dataAccount: dataAccount.publicKey })
-    .remainingAccounts([
-      {
-        pubkey: wallet.publicKey,
-        isWritable: true,
-        isSigner: true,
-      },
-      {
-        pubkey: mintKeypair.publicKey,
-        isWritable: false,
-        isSigner: false,
-      },
-      {
-        pubkey: tokenAccount.address,
-        isWritable: true,
-        isSigner: false,
-      },
-      {
-        pubkey: receipientTokenAccount.address,
-        isWritable: true,
-        isSigner: false,
-      },
-    ])
-    .rpc();
-  console.log("Your transaction signature", tx);
-});
 
 async function getRecipientTokenAmount() {
   // Assuming getAccount and receipientTokenAccount are defined elsewhere
@@ -217,6 +135,115 @@ async function getRecipientTokenAmount() {
   // let tokens = Number(recepienttokenAmount);
   // assert.equal(tokens / LAMPORTS_PER_SOL, 54);
 
+  it("Transfer some tokens to another wallet!", async () => {
+    // Wallet's associated token account address for mint
+    // const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    //   connection,
+    //   wallet.payer, // payer
+    //   mintKeypair.publicKey, // mint
+    //   wallet.publicKey // owner
+    // );
+
+    const receipient = anchor.web3.Keypair.generate();
+    const receipientTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      wallet.payer, // payer
+      mintKeypair.publicKey, // mint account
+      receipient.publicKey // owner account
+    );
+
+    const tx = await program.methods
+      .transferTokens(
+        new anchor.BN(50)
+      )
+      .accounts({
+        from: tokenAccount.address,
+        to: receipientTokenAccount.address,
+        owner: wallet.publicKey,
+      })
+      .rpc();
+    console.log("Your transaction signature", tx);
+    const recepienttokenAmount = (await getAccount(connection, receipientTokenAccount.address)).amount;
+    console.log("recipienttokenAmount", recepienttokenAmount);
+    let tokens = Number(recepienttokenAmount);
+    console.log("reciepenmt token:", tokens);
+
+
+    const bachehuetoken = (await getAccount(connection, tokenAccount.address)).amount;
+    console.log("bachehuetoken", bachehuetoken);
+    let tokensss = Number(bachehuetoken);
+    console.log("tkens", tokensss)
+
+
+    //  assert.equal(tokens / LAMPORTS_PER_SOL, 150);
+    // });
+  });
+
+
+
+
+
+
+  console.log("helo")
+
+
+  // it("Transfer some tokens to another wallet!", async () => {
+  //   // Wallet's associated token account address for mint
+  //   const tokenAccount = await getOrCreateAssociatedTokenAccount(
+  //     connection,
+  //     wallet.payer, // payer
+  //     mintKeypair.publicKey, // mint
+  //     wallet.publicKey // owner
+  //   );
+
+  //   const receipient = anchor.web3.Keypair.generate();
+  //   const receipientTokenAccount = await getOrCreateAssociatedTokenAccount(
+  //     connection,
+  //     wallet.payer, // payer
+  //     mintKeypair.publicKey, // mint account
+  //     receipient.publicKey // owner account
+  //   );
+
+  //  // console.log("receipientTokenAccount",receipientTokenAccount)
+
+  //   const tx = await program.methods
+  //     .transferTokens(
+  //       tokenAccount.address,
+  //       receipientTokenAccount.address,
+  //       new anchor.BN(54000000000)
+  //     )
+  //     .accounts({ dataAccount: dataAccount.publicKey })
+  //     .remainingAccounts([
+  //       {
+  //         pubkey: wallet.publicKey,
+  //         isWritable: true,
+  //         isSigner: true,
+  //       },
+  //       {
+  //         pubkey: mintKeypair.publicKey,
+  //         isWritable: false,
+  //         isSigner: false,
+  //       },
+  //       {
+  //         pubkey: tokenAccount.address,
+  //         isWritable: true,
+  //         isSigner: false,
+  //       },
+  //       {
+  //         pubkey: receipientTokenAccount.address,
+  //         isWritable: true,
+  //         isSigner: false,
+  //       },
+  //     ])
+  //     .rpc();
+  //   console.log("Your transaction signature", tx);
+
+  //   const recepienttokenAmount = (await getAccount(connection, receipientTokenAccount.address)).amount;
+  //   console.log("recipienttokenAmount", recepienttokenAmount);
+  //   let tokens = Number(recepienttokenAmount);
+
+  //   assert.equal(tokens / LAMPORTS_PER_SOL, 54);
+
+
+
 });
-
-
